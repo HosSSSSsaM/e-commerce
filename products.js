1bc741ad0d1fcefee3c41e02920bc1322a33a4b1
HosSSSSsaM <hossamelazony18@gmail.com>
2024-02-15 12:13:36 +0200

    testt

--- products.js
+++ products.js
@@ -16,70 +16,80 @@ var products = [
         img : "images/vegies.png",
         name : "Fresh vagies",
         price : 30,
-        quantity : 0
+        quantity : 0,
+        totall:30,
     },
     {
         id : 2,
         img : "images/yogurt.png",
         name : "Yogurt",
         price : 35,
-        quantity : 0
+        quantity : 0,
+        totall:35,
     },
     {
         id : 3,
         img : "images/plate-1.png",
         name : "Summer salad",
         price : 40,
-        quantity : 0
+        quantity : 0,
+        totall:0,
     },
     {
         id : 4,
         img : "images/plate-2.png",
         name : "Cottage dish",
         price : 45,
-        quantity : 0
+        quantity : 0,
+         totall:45,
     },
     {
         id : 5,
         img : "images/plate-3.png",
         name : "Greek salad",
         price : 50,
-        quantity : 0
+        quantity : 0,
+         totall:50,
     },
     {
         id : 6,
         img : "images/salad-table.jpg",
         name : "paradise dish",
         price : 55,
-        quantity : 0
+        quantity : 0,
+         totall:55,
     },
     {
         id : 7,
         img : "images/coffee.jpg",
         name : "Coffee time",
         price : 60,
-        quantity : 0
+        quantity : 0,
+         totall:60,
     },
     {
         id : 8,
         img : "images/food-table.jpg",
         name : "Dinner time",
         price : 70,
-        quantity : 0
+        quantity : 0,
+         totall:70,
     },
     {
         id : 9,
         img : "images/jars.jpg",
         name : "After dinner ",
         price : 100,
-        quantity : 0
+        quantity : 0,
+         totall:100,
     },
     {
         id : 10,
         img : "images/cupcake.png",
         name : "Cup cake",
         price : 110,
-        quantity : 0
+        quantity : 0,
+         totall:110,
     },
 ]
 
@@ -127,35 +137,6 @@ function addtocart(e){
     
 }
 
-function show_popup(e){
-console.log(products[e].name);
-    let blur = document.createElement("div")
-    blur.classList.add("blur")
-    document.querySelector("body").prepend(blur)
-
-    let popup_box = document.createElement("div");
-    popup_box.classList.add("popup_box")
-    document.querySelector("body").prepend(popup_box)
-    let text = document.createElement("h3")
-    text.innerHTML = ` your product <span>(${products[e].name})</span> add to cart successfuly`
-    popup_box.append(text)
-    let img = document.createElement("img")
-    img.src = "404-tick.png"
-    popup_box.append(img)
-    let close = document.createElement("h6")
-    close.innerHTML = "X"
-    popup_box.append(close)
-
-    close.onclick = function(){
-        blur.remove()
-        popup_box.remove()
-    }
-    setTimeout(() => {
-        blur.remove()
-        popup_box.remove()
-    }, 3000);
-
-}
 
 
 function show_products_in_cart(e){
@@ -177,7 +158,7 @@ function show_products_in_cart(e){
                         <img src="${value2.img}" alt="">
                         <div>
                             <p>${value2.name}</p>
-                            <p>$${value2.price}.00</p>
+                            <p>$${value2.totall}.00</p>
                         </div>
                     </div>
                     <div class="details_two">
@@ -199,6 +180,40 @@ function show_products_in_cart(e){
 
 
 }
+
+
+function show_popup(e){
+    console.log(products[e].name);
+        let blur = document.createElement("div")
+        blur.classList.add("blur")
+        document.querySelector("body").prepend(blur)
+    
+        let popup_box = document.createElement("div");
+        popup_box.classList.add("popup_box")
+        document.querySelector("body").prepend(popup_box)
+        let text = document.createElement("h3")
+        text.innerHTML = ` your product <span>(${products[e].name})</span> add to cart successfuly`
+        popup_box.append(text)
+        let img = document.createElement("img")
+        img.src = "404-tick.png"
+        popup_box.append(img)
+        let close = document.createElement("h6")
+        close.innerHTML = "X"
+        popup_box.append(close)
+    
+        close.onclick = function(){
+            blur.remove()
+            popup_box.remove()
+        }
+        setTimeout(() => {
+            blur.remove()
+            popup_box.remove()
+        }, 3000);
+    
+}
+
+
+    
 function changequantity(key , quantity ,id){
     // console.log(cart[key]);
     console.log(key);
@@ -206,12 +221,14 @@ function changequantity(key , quantity ,id){
         cart.splice(key,1)
     }else{
         cart[key].quantity = quantity ;
-        cart[key].price = quantity * products[id -1].price;
+        // cart[key].price = quantity * products[id -1].price;
+        cart[key].totall = quantity * cart[key].price;
     }
     show_products_in_cart()
-    console.log(cart[key]);
+    // console.log(cart[key]);
     console.log(cart[key].price);
-    console.log(products[id - 1]);
+    console.log(quantity);
+    // console.log(products[id - 1]);
     console.log(products[id - 1].price);
 }
 
@@ -223,7 +240,7 @@ function deletee(e){
     console.log(cart);
     show_products_in_cart(e)
     if(cart.length == 0){
-        localStorage.product =JSON.stringify(cart)
+        // localStorage.product =JSON.stringify(cart)
         console.log("clear done");
         show_products_in_cart()
     }
